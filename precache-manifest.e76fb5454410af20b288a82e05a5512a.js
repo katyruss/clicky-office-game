c75253a52fe5135b256e362ad8ba5458e46cda33
Kathryn Russell <katy@Kathryns-MacBook-Pro.local>
2019-05-18 14:02:34 -0400

    Updates

---
+++ precache-manifest.e76fb5454410af20b288a82e05a5512a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eee136b9fb01296dc3ed5dcc228a17b",
+    "url": "/clicky-office-game/index.html"
+  },
+  {
+    "revision": "8d8ffe5bfcf82104f3de",
+    "url": "/clicky-office-game/static/css/main.5f976f22.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/clicky-office-game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "8d8ffe5bfcf82104f3de",
+    "url": "/clicky-office-game/static/js/main.a28f6fca.chunk.js"
+  },
+  {
+    "revision": "4a03c930870635e69970",
+    "url": "/clicky-office-game/static/js/runtime~main.ea31a967.js"
+  }
+]);
